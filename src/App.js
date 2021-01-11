5ef2297496f982d903400f19cae8726e941ee24f
Marvin T <contact@marvinkturner.com>
2021-01-10 23:59:12 -0500

    wip: fetch data from api

--- src/App.js
+++ src/App.js
@@ -4,9 +4,36 @@ import Tours from './Tours';
 
 const url = 'https://course-api.com/react-tours-project';
 function App() {
+  // set up hooks
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
-  return <h2>Tours Project Setup</h2>;
+
+  const fetchTours = async () => {
+    setLoading(true);
+  };
+
+  const fetchTours = async () => {
+    setLoading(true);
+    const res = await fetch(url);
+    const tours = await fetch.json();
+    console.log(tours);
+  };;
+
+  // conditional return, if fetching data
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  // default return
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
 export default App;
